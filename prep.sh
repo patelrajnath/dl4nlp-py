070b6a6b5510f82a006590f3c39fa0705d2660cc
Raj Nath Patel <patelrajnath@gmail.com>
2019-08-09 08:24:49 +0100

    Updated python version.

--- prep.sh
+++ prep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 TEXT=examples/pos/icon2016/
-python3.6 preprocess.py --source-lang txt --target-lang tags \
+python3 preprocess.py --source-lang txt --target-lang tags \
             --trainpref $TEXT/hi-en.train --validpref $TEXT/hi-en.dev --testpref $TEXT/hi-en.test \
                 --destdir data-bin/
