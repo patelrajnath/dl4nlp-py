20c2b060078110304cbabffb938402486d3fb035
Raj Nath Patel <patelrajnath@gmail.com>
2019-08-15 12:28:00 +0100

    Added lstm and cnn as registered model.

--- dl4nlp/models/lstm.py
+++ dl4nlp/models/lstm.py
@@ -1,10 +1,39 @@
 from torch import nn
 import torch.nn.functional as F
 
+from dl4nlp.models import register_model, BaseModel, register_model_architecture
+
+
+@register_model("lstm")
+class GRU(BaseModel):
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def build_model(self, args, task):
+        # make sure all arguments are present in older models
+        base_architecture(args)
+        self.embed_dim = args.encoder_embed_dim
+        src_dict, tgt_dict = task.source_dictionary, task.target_dictionary
+        context = 5
+        N = args.encoder_layers
+        d_model = args.encoder_embed_dim
+        dropout = args.dropout
+        bidirectional = args.encoder_bidirectional
+        model = LSTMTagger(num_layers = N,
+                         context=context,
+                         embedding_dim=self.embed_dim,
+                         hidden_dim=d_model,
+                         vocab_size= len(src_dict),
+                         tagset_size=len(tgt_dict),
+                         dropout=dropout,
+                         bidirectional=bidirectional)
+        return model
+
 
 class LSTMTagger(nn.Module):
 
-    def __init__(self, num_layers, context, embedding_dim, hidden_dim, vocab_size, tagset_size, bidirectional=False):
+    def __init__(self, num_layers, context, embedding_dim, hidden_dim, vocab_size, tagset_size, dropout, bidirectional=False):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
 
@@ -14,7 +43,7 @@ class LSTMTagger(nn.Module):
         # with dimensionality hidden_dim.
         self.num_layers=num_layers
         self.bidirectional=bidirectional
-        self.dropout_out = 0.01
+        self.dropout_out = dropout
         self.context = context
 
         self.lstm = nn.LSTM(
@@ -37,3 +66,16 @@ class LSTMTagger(nn.Module):
         tag_space = self.hidden2tag(lstm_out.view(n_tokens, -1))
         tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
+
+
+@register_model_architecture('lstm', 'lstm')
+def base_architecture(args):
+    args.dropout = getattr(args, 'dropout', 0.1)
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
+    args.encoder_embed_path = getattr(args, 'encoder_embed_path', None)
+    args.encoder_freeze_embed = getattr(args, 'encoder_freeze_embed', False)
+    args.encoder_hidden_size = getattr(args, 'encoder_hidden_size', args.encoder_embed_dim)
+    args.encoder_layers = getattr(args, 'encoder_layers', 1)
+    args.encoder_bidirectional = getattr(args, 'encoder_bidirectional', False)
+    args.encoder_dropout_in = getattr(args, 'encoder_dropout_in', args.dropout)
+    args.encoder_dropout_out = getattr(args, 'encoder_dropout_out', args.dropout)
